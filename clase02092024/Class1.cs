a916728026d179400f91e1868641455aac4c3459
marti <martinagoncalvezz@gmail.com>
2024-12-07 15:20:40 -0300

    Agregar archivos de proyecto.

---
+++ clase02092024/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase02092024
+{
+    internal class Auto
+    {
+        private string matricula;
+        private string marca;
+        private string color;
+
+        public Auto(string matricula, string marca, string color)
+        {
+            this.matricula = matricula;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        
+        public string GetMatricula()
+        {
+            return matricula;
+        }
+
+       
+        public string GetMarca()
+        {
+            return marca;
+        }
+
+       
+        public string GetColor()
+        {
+            return color;
+        }
+
+        
+        public void Pintar(string nuevoColor)
+        {
+            color = nuevoColor;
+        }
+    }
+}
---
+++ clase02092024/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase02092024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Auto vw = new Auto("AZA2991", "Volkswagen", "rojo");
+            Auto citroen = new Auto("SAS2344", "Citroën", "verde");
+
+            
+            vw.Pintar("azul");
+
+            
+            Console.WriteLine($"Auto 1: Marca = {vw.GetMarca()}, Matrícula = {vw.GetMatricula()}, Color = {vw.GetColor()}");
+            Console.WriteLine($"Auto 2: Marca = {citroen.GetMarca()}, Matrícula = {citroen.GetMatricula()}, Color = {citroen.GetColor()}");
+        }
+    }
+}
